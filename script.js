3c7e1875beca017cec2708c92d22db864533ca6b
Bazuk1 <64947923+Bazuk1@users.noreply.github.com>
2020-06-19 11:32:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function Start() {
     var H = document.getElementById("hours").value;
     var M = document.getElementById("minutes").value;
     var S = document.getElementById("seconds").value;
-    document.getElementById("title").innerHTML = Name;
+    document.getElementById("title").innerHTML = "Countdown for " + Name;
     endDate = new Date();
     endDate.setUTCDate(endDate.getUTCDate() + Number(D));
     endDate.setUTCHours(endDate.getUTCHours() + Number(H));
@@ -23,16 +23,20 @@ function Start() {
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         document.getElementById("Result").innerHTML = " Days: "+ days +" Hours: " + hours + " Minutes: " + minutes + " Seconds: " + seconds; 
+        if (Name === "") {
+            document.getElementById("title").innerHTML = "Countdown for No Name";
+        }
         if (distance < 250) {
             clearInterval(x);
-            document.getElementById("Result").innerHTML = "Now!"; 
+            document.getElementById("Result").innerHTML = "Finished!"; 
+            playAlarm();
         }
       }, 1000);
 
 }
 function Reset() {
     document.getElementById("title").innerHTML = "Create Your Own Countdown!";
-   
+    audio.pause();
     endDate = 0;
     distance = 0;
     document.getElementById("Countdowns").style.display = "flex";
@@ -44,4 +48,9 @@ function Reset() {
     document.getElementById("hours").value = "";
     document.getElementById("minutes").value = "";
     document.getElementById("seconds").value = "";
+}
+
+var audio = document.getElementById("audio");
+function playAlarm() {
+audio.play();
 }
